be422417f43e8d54cf40e70cc9e74d32b03ac2ba
isaac <isw9@case.edu>
2018-10-25 16:51:16 -0400

    debug

--- irb120_reactive_task_commander.cpp
+++ irb120_reactive_task_commander.cpp
@@ -172,7 +172,7 @@ int main(int argc, char** argv) {
 
     //function call from library (Class) CartTrajPlanner: converts a joint-space path to a joint-space trajectory
     pCartTrajPlanner->path_to_traj(optimal_path, arrival_time, new_trajectory);
-    print_traj(new_trajectory); //display for  debug
+    //print_traj(new_trajectory); //display for  debug
 
     traj_publisher.publish(new_trajectory); //publish the trajectory;
     ros::Duration(1).sleep();
